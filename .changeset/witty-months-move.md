6d5429b40bc3d0eab8b7a86b441641eaea3de2f5
Luma <world@luma.email>
2021-09-14 19:45:31 +0900

    docs(changeset): change config interface alongside base

---
+++ .changeset/witty-months-move.md
@@ -0,0 +1,5 @@
+---
+'@luma-dev/eslint-config-svelte': minor
+---
+
+change config interface alongside base
--- packages/eslint-config-svelte/configure.js
+++ packages/eslint-config-svelte/configure.js
@@ -1,14 +1,38 @@
+const { convertUnresolvableToPatterns } = require('@luma-dev/eslint-config-base/helpers');
+
 /**
- * @param {string} dirname
+ * @param {import('@luma-dev/eslint-config-base').Config} config
  * @return {import('eslint').Linter.ConfigOverride[]}
  */
-const configureSvelte = (dirname) => [
-  {
-    files: ['*.svelte'],
-    parserOptions: {
-      tsconfigRootDir: dirname,
+const configureSvelte = (config) => {
+  const { __dirname, unresolvable } = config;
+  return [
+    {
+      files: ['*.svelte'],
+      parserOptions: {
+        tsconfigRootDir: __dirname,
+      },
+      rules: {
+        'import/no-unresolved': [
+          'error',
+          {
+            ignore: ['^[^.].*\\.(?:css)$', ...convertUnresolvableToPatterns(unresolvable)],
+          },
+        ],
+      },
     },
-  },
-];
+    {
+      files: ['*.ts'],
+      rules: {
+        'import/no-unresolved': [
+          'error',
+          {
+            ignore: ['^[^.].*\\.(?:css)$', ...convertUnresolvableToPatterns(unresolvable)],
+          },
+        ],
+      },
+    },
+  ];
+};
 
 module.exports = configureSvelte;
